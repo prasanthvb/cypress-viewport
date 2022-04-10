3fcdec18606644bf4b131a5f0c5de7c5a64c14fb
Prasanth VB <prasanth.vb@techwondoe.com>
2022-04-10 20:52:07 +0530

    cross browser testing updated

--- cypress/integration/Testcases/BrowserTest.spec.js
+++ cypress/integration/Testcases/BrowserTest.spec.js
@@ -3,7 +3,22 @@
 import * as data from "../../fixtures/credentials.json";
 
 describe("Running test in web Browser", () => {
-  it("Test to verify userName in user profile ", () => {
+  it('Test to verify web view in Electron browser',{ browser: 'electron' }, () => {
+    cy.login(data.email, data.password);
+    cy.webLandingPageTest();
+  });
+
+  it('Test to verify web view in firefox browser',{ browser: 'firefox' }, () => {
+    cy.login(data.email, data.password);
+    cy.webLandingPageTest();
+  });
+
+  it('Test to verify web view in chrome browser',{ browser: 'chrome' }, () => {
+    cy.login(data.email, data.password);
+    cy.webLandingPageTest();
+  });
+
+  it('Test to verify web view in edge browser',{ browser: 'edge' }, () => {
     cy.login(data.email, data.password);
     cy.webLandingPageTest();
   });
